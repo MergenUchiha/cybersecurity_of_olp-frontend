d3d174026f3ce86576c0236cab671abc44fac362
Uchiha <macos@Uchiha.local>
2026-04-27 21:43:45 +0500

    vite fixed

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,18 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import basicSsl from "@vitejs/plugin-basic-ssl";
 
 export default defineConfig({
-  plugins: [react(), basicSsl()],
+  plugins: [react()],
   server: {
     host: "0.0.0.0",
     port: 5173,
-    https: {},
     proxy: {
       "/api": {
-        target: "http://157.173.103.216:6000",
+        target: "http://localhost:6000",
         changeOrigin: true,
       },
       "/video-socket": {
-        target: "http://157.173.103.216:6000",
+        target: "http://localhost:6000",
         changeOrigin: true,
         ws: true,
         secure: false,
